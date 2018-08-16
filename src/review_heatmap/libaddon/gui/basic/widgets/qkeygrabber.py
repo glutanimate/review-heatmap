330ea18db9087e96832a73f98bd40429bda85e00
Glutanimate <glutanimate@users.noreply.github.com>
2018-08-16 23:51:37 +0200

    Refactor and overhaul QKeyGrabButton
    - Move away from showInfo in favor of updating dialog label
    - Introduce platfomr-specific key strings
    - etc.

--- src/review_heatmap/libaddon/gui/basic/widgets/qkeygrabber.py
+++ src/review_heatmap/libaddon/gui/basic/widgets/qkeygrabber.py
@@ -5,62 +5,84 @@ Copyright: (c) 2018 Glutanimate <https://glutanimate.com/>
 License: GNU AGPLv3 <https://www.gnu.org/licenses/agpl.html>
 """
 
-from .qt import QDialog, QPushButton, QVBoxLayout, QLabel, Qt
-from .utils import showInfo
+from ....utils.platform import PLATFORM
+
+from .qt import QDialog, QPushButton, QVBoxLayout, QLabel, Qt, QKeySequence
+
 
 class QKeyGrabButton(QPushButton):
-    def __init__(self, text, parent=None):
-        super(QKeyGrabButton, self).__init__(text, parent=parent)
+    def __init__(self, parent=None, key_string=""):
+        super(QKeyGrabButton, self).__init__("", parent=parent)
+        self.setKey(key_string)
         self.clicked.connect(self.grabKey)
+
+    def setKey(self, key_string):
+        self.key_string = key_string
+        qkeyseq = QKeySequence(key_string, QKeySequence.PortableText)
+        native_key_string = qkeyseq.toString(format=QKeySequence.NativeText)
+        self.setText(native_key_string)
+    
+    def key(self):
+        return self.key_string
 
     def grabKey(self):
         """Invoke key grabber"""
         grabber = QKeyGrab(self.parent())
-        ret = grabber._exec()
-        # if not ret...
-        print(ret)
+        ret = grabber.exec_()
+        if ret != 1:
+            return
         key_string = grabber.key_string
         if not key_string:  # or not ret
             return
-        self.setText(key_string)
+        self.setKey(key_string)
 
 
 class QKeyGrab(QDialog):
     """
     Simple key combination grabber for hotkey assignments
 
-    Largely based on ImageResizer by searene
+    Based in part on ImageResizer by searene
     (https://github.com/searene/Anki-Addons)
     """
 
-    def __init__(self, parent, callback):
+    platform_modkey_names = {
+        "lin": {"meta": "Meta", "ctrl": "Ctrl",
+                "alt": "Alt", "shift": "Shift"},
+        "win": {"meta": "Win", "ctrl": "Ctrl", "alt":
+                "Alt", "shift": "Shift"},
+        "mac": {"meta": "Control", "ctrl": "Command",
+                "alt": "Option", "shift": "Shift"}
+    }
+
+    modkey_names = platform_modkey_names[PLATFORM]
+
+    def __init__(self, parent):
         """
         Initialize dialog
 
         Arguments:
             parent {QWidget} -- Parent Qt widget
-            callback {function} -- Callable to run once dialog exits.
-                                   Needs to be able to take a hotkey
-                                   string as an argument.
         """
-
         QDialog.__init__(self, parent=parent)
         self.parent = parent
-        self.callback = callback
         # self.active is used to trace whether there's any key held now:
         self.active = 0
-        self.meta = self.ctrl = self.alt = self.shift = False
-        self.extra = self.key_string = None
+        self.resetDialog()
         self.setupUI()
 
     def setupUI(self):
         """Basic UI setup"""
         mainLayout = QVBoxLayout()
-        label = QLabel("Please press they key combination\n"
-                       "you would like to assign")
-        mainLayout.addWidget(label)
+        self.label = QLabel("Please press the key combination\n"
+                            "you would like to assign")
+        self.label.setAlignment(Qt.AlignCenter)
+        mainLayout.addWidget(self.label)
         self.setLayout(mainLayout)
         self.setWindowTitle("Grab key combination")
+
+    def resetDialog(self):
+        self.extra = self.key_string = None
+        self.meta = self.ctrl = self.alt = self.shift = False
 
     def keyPressEvent(self, evt):
         """
@@ -71,7 +93,7 @@ class QKeyGrab(QDialog):
             evt {QKeyEvent} -- Intercepted key press event
         """
         self.active += 1
-        
+
         key = evt.key()
         if key > 0 and key < 127:
             self.extra = chr(key)
@@ -99,17 +121,24 @@ class QKeyGrab(QDialog):
             return
 
         # TODO: platform-specific messages
+        msg = None
         if not (self.shift or self.ctrl or self.alt or self.meta):
-            showInfo("Please use at least one keyboard modifier\n"
-                     "(Win, Ctrl, Alt, Shift)")
-            return
+            msg = ("Please use at least one keyboard modifier\n"
+                   "({meta}, {ctrl}, {alt}, {shift})".format(
+                       **self.modkey_names))
         if (self.shift and not (self.ctrl or self.alt or self.meta)):
-            showInfo("Shift needs to be combined with at least one\n"
-                     "other modifier (Win, Ctrl, Alt)")
-            return
+            msg = ("Shift needs to be combined with at least one\n"
+                   "other modifier ({meta}, {ctrl}, {alt})".format(
+                       **self.modkey_names))
         if not self.extra:
-            showInfo("Please press at least one key that is \n"
-                     "not a modifier (not Win, Ctrl, Alt, or Shift)")
+            msg = ("Please press at least one key that is \n"
+                   "not a modifier (not {meta}, {ctrl}, "
+                   "{alt}, or {shift})".format(
+                       **self.modkey_names))
+
+        if msg:
+            self.label.setText(msg)
+            self.resetDialog()
             return
 
         combo = []
@@ -124,8 +153,5 @@ class QKeyGrab(QDialog):
         combo.append(self.extra)
 
         self.key_string = "+".join(combo)
-        # TODO: Show key string according to platform-specific key designations:
-        # keySeq = QKeySequence(key_string)
-        # key_string = keySeq.toString(format=QKeySequence.NativeText)
 
         self.accept()
