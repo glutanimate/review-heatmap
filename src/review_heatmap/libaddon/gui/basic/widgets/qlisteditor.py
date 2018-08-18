83b2f2dc55076d1af4ca8b6fe0efdea073e7b19c
Glutanimate <glutanimate@users.noreply.github.com>
2018-08-18 23:52:36 +0200

    First implementation of a custom list widget with editing buttons

---
+++ src/review_heatmap/libaddon/gui/basic/widgets/qlisteditor.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+Icons: plus.svg, minus.svg
+
+Copyright: (c) 2018 Glutanimate <https://glutanimate.com/>
+License: GNU AGPLv3 <https://www.gnu.org/licenses/agpl.html>
+"""
+
+from .qt import *
+
+from ....utils.platform import ADDON_MODULE
+
+class QListEditor(QGroupBox):
+
+    def __init__(self, *args, **kwargs):
+        super(QListEditor, self).__init__(*args, **kwargs)
+        self.setAlignment(Qt.AlignHCenter)
+        self._setupWidgets()
+
+    def _setupWidgets(self):
+        hlayout = QHBoxLayout(self)
+        vlayout = QVBoxLayout(self)
+
+        self.list = QListWidget(self)
+        self.list.setSelectionMode(QAbstractItemView.ExtendedSelection)
+
+        for name, icon in (("btnAdd", "plus"), ("btnRem", "minus")):
+            setattr(self, name, QToolButton(self))
+            btn = getattr(self, name)
+            btn.setMinimumSize(QSize(32, 0))
+            iconAdd = QIcon()
+            iconAdd.addPixmap(QPixmap(":/{}/icons/{}.svg".format(
+                ADDON_MODULE, icon)), QIcon.Normal, QIcon.Off)
+            btn.setIcon(iconAdd)
+            btn.setIconSize(QtCore.QSize(16, 16))
+
+        spacer = QSpacerItem(
+            20, 40, QtWidgets.QSizePolicy.Minimum,
+            QtWidgets.QSizePolicy.Expanding)
+        hlayout.addWidget(self.list)
+        vlayout.addWidget(self.btnAdd)
+        vlayout.addWidget(self.btnRem)
+        vlayout.addItem(spacer)
+        hlayout.addLayout(vlayout)
