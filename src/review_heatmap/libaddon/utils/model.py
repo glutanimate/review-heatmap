f4cc7d896b010d26fb0c1a1c9677de4ec245b9a3
Glutanimate <glutanimate@users.noreply.github.com>
2018-08-19 19:26:56 +0200

    Start working on a note type / template manager

---
+++ src/review_heatmap/libaddon/utils/model.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright: (c) 2018 Glutanimate <https://glutanimate.com/>
+License: GNU AGPLv3 <https://www.gnu.org/licenses/agpl.html>
+"""
+
+from .platform import ANKI21, ADDON_PATH, ADDON_MODULE
+
+class ModelManager(object):
+
+    def __init__(self, model_name, fields, templates, css,
+                 template_updates=[]):
+        pass
+    
+    
