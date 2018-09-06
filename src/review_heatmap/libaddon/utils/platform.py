7e4f56cee834a44ef2e03326629722e7f44d02a9
Glutanimate <glutanimate@users.noreply.github.com>
2018-09-06 22:20:04 +0200

    Refactor add-on name determination. Keep track of library name.

--- src/review_heatmap/libaddon/utils/platform.py
+++ src/review_heatmap/libaddon/utils/platform.py
@@ -11,23 +11,24 @@ from aqt import mw
 from anki import version
 from anki.utils import isMac, isWin
 
-def addonsFolder():
-    path = mw.pm.addonFolder()
-    if isWin:
-        path = path.encode(sys.getfilesystemencoding())
-    return path
-
 PYTHON3 = sys.version_info[0] == 3
 ANKI21 = version.startswith("2.1.")
 SYS_ENCODING = sys.getfilesystemencoding()
 
-ADDON_MODULE = __name__.split(".")[0]
+name_components = __name__.split(".")
+
+ADDON_MODULE = name_components[0]
+LIBADDON_NAME = name_components[1]
+
 if ANKI21:
     ADDON_DIR = mw.addonManager.addonsFolder()
     JSPY_BRIDGE = "pycmd"
 else:
-    ADDON_DIR = addonsFolder()
+    ADDON_DIR = mw.pm.addonFolder()
+    if isWin:
+        ADDON_DIR = ADDON_DIR.encode(SYS_ENCODING)
     JSPY_BRIDGE = "py.link"
+
 ADDON_PATH = os.path.join(ADDON_DIR, ADDON_MODULE)
 
 if isMac:
