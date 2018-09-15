8906c73fc820b67766f33b875909b02507a2d32a
Glutanimate <glutanimate@users.noreply.github.com>
2018-09-15 21:06:18 +0200

    Move third-party module import code to separate module

--- src/review_heatmap/libaddon/__init__.py
+++ src/review_heatmap/libaddon/__init__.py
@@ -1,47 +1,31 @@
+# -*- coding: utf-8 -*-
+
 """
-Package for common reusable modules that can be
-deployed across multiple add-ons.
+Libaddon: A helper library for Anki add-on development
+
+Provides access to a number of commonly used modules shared across
+many of my add-ons. Probably not fit for general use yet, as it likely
+still is too specific to my own add-ons and implementations.
+
+This module is the package entry-point.
+
+Copyright: (c) 2016-2018 Glutanimate <https://glutanimate.com/>
+License: GNU AGPLv3 <https://www.gnu.org/licenses/agpl.html>
 """
 
 from __future__ import unicode_literals
 
-import sys
 import os
 
-from .utils.platform import PYTHON3, ADDON_PATH, LIBADDON_NAME
 from ._version import __version__  # noqa: F401
 
-# Add local libraries to Python module search path
+from .utils.platform import PATH_ADDON, MODULE_LIBADDON
+from .utils.packaging import addSubdirPathToModuleLookup
 
-module_search_paths = []
 
-# 1.) Shipped with library:
-module_search_paths.append(os.path.join(ADDON_PATH, LIBADDON_NAME, "libs",
-                           "python3" if PYTHON3 else "python2"))
-module_search_paths.append(os.path.join(ADDON_PATH, LIBADDON_NAME, "libs",
-                           "common"))
-# 2.) Shipped with add-on:
-module_search_paths.append(os.path.join(ADDON_PATH, "libs",
-                           "python3" if PYTHON3 else "python2"))
-module_search_paths.append(os.path.join(ADDON_PATH, "libs",
-                           "common"))
+__all__ = [
+    "__version__"
+]
 
-# Insert at idx 0 in order to supersede system-wide packages
-for path in module_search_paths:
-    if not os.path.isdir(path):
-        continue
-    sys.path.insert(0, path)
-
-# NOTE: I have yet to find a reliable way to add modules to packages
-# that *do* ship with Anki, but are missing specific sub-modules
-# (e.g. 'version' module of 'distutils')
-#
-# The issue mainly arises when Anki has already loaded the corresponding
-# module at add-on init time. At that point it becomes non-trivial to
-# update the module cache with our own version of the module.
-#
-# It is easy to make due with explicitly importing our own version of
-# the module if we have control over the code-base, but in case of
-# dependencies of other modules this becomes a major problem
-# (e.g. third-party packages depending on stdlib modules missing
-# in Anki's Python distribution).
+# Add libraries shipped with libaddon to Python module lookup
+addSubdirPathToModuleLookup(os.path.join(PATH_ADDON, MODULE_LIBADDON, "libs"))
---
+++ src/review_heatmap/libaddon/utils/packaging.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright: (c) 2018 Glutanimate <https://glutanimate.com/>
+License: GNU AGPLv3 <https://www.gnu.org/licenses/agpl.html>
+"""
+
+import sys
+import os
+
+from .platform import PYTHON3
+
+__all__ = [
+    "addPathToModuleLookup",
+    "addSubdirPathToModuleLookup"
+]
+
+STRINGTYPES = (str,) if PYTHON3 else (str, unicode)  # noqa: F821
+LOOKUP_SUBDIRS = ["libs", "python3" if PYTHON3 else "python2"]
+
+
+# Registering external libraries & modules
+######################################################################
+
+# NOTE: I have yet to find a reliable way to add modules to packages
+# that *do* ship with Anki, but are missing specific sub-modules
+# (e.g. 'version' module of 'distutils')
+#
+# The issue mainly arises when Anki has already loaded the corresponding
+# module at add-on init time. At that point it becomes non-trivial to
+# update the module cache with our own version of the module.
+#
+# It is easy to make due with explicitly importing our own version of
+# the module if we have control over the code-base, but in case of
+# dependencies of other modules this becomes a major problem
+# (e.g. third-party packages depending on stdlib modules missing
+# in Anki's Python distribution).
+
+def _addPathToModuleLookup(path):
+    # Insert at idx 0 in order to supersede system-wide packages
+    sys.path.insert(0, path)
+
+def addPathToModuleLookup(path):
+    """
+    Add modules shipped with the add-on to Python module search path
+    
+    Arguments:
+        path {str,unicode} -- Fully qualified path to module directory
+    """
+    assert isinstance(path, STRINGTYPES)
+    assert os.path.isdir(path)
+    _addPathToModuleLookup(path)
+
+def addSubdirPathToModuleLookup(path):
+    """
+    Recursively add module directory with version-specific subfolders
+    to Python module search path
+
+    Arguments:
+        path {str,unicode} -- Fully qualified path to module directory with
+                              one or more of the following subfolders:
+                              python2, python3, common
+    """
+    assert isinstance(path, STRINGTYPES)
+    assert os.path.isdir(path)
+    for path in [os.path.join(path, subdir) for subdir in LOOKUP_SUBDIRS]:
+        if not os.path.isdir(path):
+            continue
+        _addPathToModuleLookup(path)
+
+
+# Installing binary dependencies (that are either part of a
+# packaged module or standalone executables)
+######################################################################
+
+class BinaryInstaller(object):
+
+    def __init__(self):
+        raise NotImplementedError
