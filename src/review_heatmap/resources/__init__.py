911cbeb57260d15a91d76fb00cfcbe1cd0b052a1
Glutanimate <glutanimate@users.noreply.github.com>
2018-10-21 17:24:08 +0200

    Decouple compiled resources from compiled forms
    Some of the resource contents are used outside of Qt dialogs
    (e.g. web components), so it makes no sense to boundle them
    with the former.

---
+++ src/review_heatmap/resources/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+This file is part of the Review Heatmap add-on for Anki
+
+Copyright: (c) 2016-2018 Glutanimate <https://glutanimate.com/>
+License: GNU AGPLv3 <https://www.gnu.org/licenses/agpl.html>
+"""
+
+from ..libaddon.platform import ANKI21
+
+if ANKI21:
+    from compiled.anki21 import *
+else:
+    from compiled.anki20 import *
+
