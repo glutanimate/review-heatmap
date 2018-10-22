dc6abe044fa3518453dfefc094db68e34ea1bf7c
Glutanimate <glutanimate@users.noreply.github.com>
2018-10-22 08:12:01 +0200

    Add support for title element

--- src/review_heatmap/libaddon/gui/about.py
+++ src/review_heatmap/libaddon/gui/about.py
@@ -34,18 +34,19 @@ html_template = """\
         li {{
             margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;
         }}
-        p {{
+        body {{
             margin-left: 10px; margin-right: 10px;
         }}
     </style>
 </head>
 <body>
+    {title}
     <p><span style="font-weight:600;">Credits</span></p>
     {authors_string}
     <p>With patches from: <i>{contributors_string}</i></p>
     {libs_string}
     
-    <p><img src=":/review_heatmap/icons/heart_small.svg"/><span style=" font-weight:600;"> Thank you!</span></p>
+    <p><img src="qrc:/review_heatmap/icons/heart_small.svg"/><span style=" font-weight:600;"> Thank you!</span></p>
     <p>My heartfelt thanks go out to everyone who has supported this add-on through their tips, contributions, or any other means. You guys rock!</p>
     <p>In particular I would like to thank all of the awesome people who support me on <a href="https://www.patreon.com/glutanimate">Patreon</a>, including:</p>
     <p><i><span style="color:#aa2a4c;">{patrons_string}</span></i></p>
@@ -64,8 +65,11 @@ libs_item_template = """\
 <li><a href="{url}">{name}</a> ({version}), © {author}, {license}</li>\
 """
 
+title_template = """\
+<h3>About {display_name}</h3>\
+"""
 
-def get_about_string():
+def get_about_string(title=False):
     authors_string = "\n".join(authors_template.format(**dct)
                                for dct in AUTHORS)
     libs_entries = "\n".join(libs_item_template.format(**dct)
@@ -80,8 +84,14 @@ def get_about_string():
     patrons_all_string = ", ".join(sorted(PATRONS, key=string.lower))
     patrons_string = ", ".join((patrons_top_string, patrons_all_string))
 
+    if title:
+        title_string = title_template.format(display_name=ADDON_NAME)
+    else:
+        title_string = ""
+
     return html_template.format(display_name=ADDON_NAME,
                                 license=LICENSE,
+                                title=title_string,
                                 authors_string=authors_string,
                                 libs_string=libs_string,
                                 contributors_string=contributors_string,
