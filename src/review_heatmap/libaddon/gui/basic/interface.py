31978e372bc53de89f1c8b2ea0a338e8df146cb3
contra-bit <liam@contra-bit.com>
2022-01-14 11:22:33 +0100

    Fix crash due to deprecation of collections in python 3.10
    DeprecationWarning: Using or importing the ABCs from 'collections' instead of from 'collections.abc' is deprecated since Python 3.3, and in 3.10 it will stop working
    
    This correctly imports from collections.abc

--- src/review_heatmap/libaddon/gui/basic/interface.py
+++ src/review_heatmap/libaddon/gui/basic/interface.py
@@ -45,7 +45,7 @@ UIs.
 from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 
-from collections import MutableSequence, MutableSet, MutableMapping
+from collections.abc import MutableSequence, MutableSet, MutableMapping
 
 from ...utils import getNestedAttribute
 from ...platform import PYTHON3
